7529ceada84b7eac55324134108bfbf58f5f77c6
Oleg Shishkin <olegshishkin@gmail.com>
2022-05-09 14:49:30 +0300

    add createdAt field for Operation entity fix type matching add optimistic locking

--- accounts/src/main/java/io/github/olegshishkin/accounting/accounts/model/Account.java
+++ accounts/src/main/java/io/github/olegshishkin/accounting/accounts/model/Account.java
@@ -1,6 +1,7 @@
 package io.github.olegshishkin.accounting.accounts.model;
 
 import static java.math.BigDecimal.ZERO;
+import static org.springframework.data.mongodb.core.mapping.FieldType.DECIMAL128;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
@@ -8,7 +9,9 @@ import java.util.Set;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @ToString
 @Data
@@ -19,9 +22,12 @@ public class Account {
   private String id;
   private String name;
   private Set<String> tags = new HashSet<>();
+  @Field(targetType = DECIMAL128)
   private BigDecimal balance;
   private String comment;
   private Boolean disabled;
+  @Version
+  private Long version;
 
   public static Account create() {
     Account a = new Account();
--- accounts/src/main/java/io/github/olegshishkin/accounting/accounts/model/Operation.java
+++ accounts/src/main/java/io/github/olegshishkin/accounting/accounts/model/Operation.java
@@ -1,11 +1,17 @@
 package io.github.olegshishkin.accounting.accounts.model;
 
+import static org.springframework.data.mongodb.core.mapping.FieldType.DECIMAL128;
+
 import java.math.BigDecimal;
+import java.time.Instant;
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @ToString
 @Data
@@ -14,11 +20,16 @@ public class Operation {
 
   @Id
   private String id;
+  @CreatedDate
+  private Instant createdAt;
   @Indexed
   private String transactionId;
   private String messageId;
   private OperationAccount account;
+  @Field(targetType = DECIMAL128)
   private BigDecimal amount;
+  @Version
+  private Long version;
 
   @ToString
   @Data

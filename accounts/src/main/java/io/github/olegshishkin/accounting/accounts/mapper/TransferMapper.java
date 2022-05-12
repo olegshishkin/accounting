cd66c07f5fd9e1d1dda503f58fbfd9244f3ec339
Oleg Shishkin <olegshishkin@gmail.com>
2022-05-12 20:12:38 +0300

    fix the creation time of transfer operations

--- accounts/src/main/java/io/github/olegshishkin/accounting/accounts/mapper/TransferMapper.java
+++ accounts/src/main/java/io/github/olegshishkin/accounting/accounts/mapper/TransferMapper.java
@@ -2,9 +2,11 @@ package io.github.olegshishkin.accounting.accounts.mapper;
 
 import io.github.olegshishkin.accounting.accounts.service.dto.Transfer;
 import io.github.olegshishkin.accounting.operation.messages.commands.CreateTransferCmd;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring",
     uses = {OperationMapper.class},
@@ -14,4 +16,13 @@ public interface TransferMapper {
   @Mapping(target = "from", source = "cmd", qualifiedByName = "mapSource")
   @Mapping(target = "to", source = "cmd", qualifiedByName = "mapDestination")
   Transfer map(CreateTransferCmd cmd);
+
+  @AfterMapping
+  default void postMapping(@MappingTarget Transfer transfer) {
+    setSameTime(transfer);
+  }
+
+  private void setSameTime(Transfer transfer) {
+    transfer.to().setCreatedAt(transfer.from().getCreatedAt());
+  }
 }

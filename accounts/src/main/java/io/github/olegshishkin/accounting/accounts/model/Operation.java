cc26317a33ddb393c0b384568c5d4e06bf8c1f45
Oleg Shishkin <olegshishkin@gmail.com>
2022-05-15 23:16:32 +0300

    add a cancellation struct to the Operation entity

--- accounts/src/main/java/io/github/olegshishkin/accounting/accounts/model/Operation.java
+++ accounts/src/main/java/io/github/olegshishkin/accounting/accounts/model/Operation.java
@@ -4,6 +4,7 @@ import static org.springframework.data.mongodb.core.mapping.FieldType.DECIMAL128
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
 import lombok.Data;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
@@ -19,16 +20,55 @@ public class Operation {
 
   @Id
   private String id;
+
+  /**
+   * Actual creation time of this entry.
+   */
   private Instant createdAt;
+
+  /**
+   * Operation date.
+   */
+  private LocalDate date;
+
+  /**
+   * An identifier of related operations that are one transaction.
+   */
   @Indexed
   private String transactionId;
+
+  /**
+   * The message identifier that is the cause of this operation.
+   */
+  @Indexed
   private String messageId;
+
+  /**
+   * Affected account.
+   */
   private OperationAccount account;
+
+  /**
+   * Operation amount.
+   */
   @Field(targetType = DECIMAL128)
   private BigDecimal amount;
+
+  /**
+   * If this field is not null, this operation has been cancelled.
+   */
+  private Cancellation cancellation;
+
   @Version
   private Long version;
 
+  public void setAccountName(Account source) {
+    this.getAccount().setName(source.getName());
+  }
+
+  /**
+   * Account.
+   */
   @ToString
   @Data
   public static class OperationAccount {
@@ -36,5 +76,20 @@ public class Operation {
     @Indexed
     String id;
     String name;
+  }
+
+  @ToString
+  @Data
+  public static class Cancellation {
+
+    /**
+     * The message identifier that caused this operation to be cancelled.
+     */
+    private String messageId;
+
+    /**
+     * Cancellation time.
+     */
+    private Instant time;
   }
 }

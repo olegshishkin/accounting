cd0aec99a0d62d571616ab98d60f20bb25fc0b07
Oleg Shishkin <olegshishkin@gmail.com>
2022-05-16 22:09:58 +0300

    update tests

--- accounts/src/test/java/io/github/olegshishkin/accounting/accounts/jms/MessageQueueListenersTest.java
+++ accounts/src/test/java/io/github/olegshishkin/accounting/accounts/jms/MessageQueueListenersTest.java
@@ -9,21 +9,25 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 
 import io.github.olegshishkin.accounting.accounts.jms.MessageQueueListenersTest.MessageQueueListenersTestConfig;
+import io.github.olegshishkin.accounting.accounts.mapper.AccountMapper;
+import io.github.olegshishkin.accounting.accounts.mapper.AccountMapperImpl;
 import io.github.olegshishkin.accounting.accounts.mapper.OperationMapper;
 import io.github.olegshishkin.accounting.accounts.mapper.OperationMapperImpl;
-import io.github.olegshishkin.accounting.accounts.mapper.TransferMapper;
-import io.github.olegshishkin.accounting.accounts.mapper.TransferMapperImpl;
+import io.github.olegshishkin.accounting.accounts.messages.commands.ApplyTransactionCmd.ApplyTransactionCmdBuilder;
+import io.github.olegshishkin.accounting.accounts.messages.commands.Entry.EntryBuilder;
+import io.github.olegshishkin.accounting.accounts.messages.commands.Entry.Type;
+import io.github.olegshishkin.accounting.accounts.messages.commands.Header.HeaderBuilder;
 import io.github.olegshishkin.accounting.accounts.model.Account;
 import io.github.olegshishkin.accounting.accounts.model.Operation;
+import io.github.olegshishkin.accounting.accounts.repository.AccountRepository;
 import io.github.olegshishkin.accounting.accounts.repository.OperationRepository;
+import io.github.olegshishkin.accounting.accounts.service.AccountService;
+import io.github.olegshishkin.accounting.accounts.service.AccountServiceImpl;
 import io.github.olegshishkin.accounting.accounts.service.OperationService;
 import io.github.olegshishkin.accounting.accounts.service.OperationServiceImpl;
-import io.github.olegshishkin.accounting.operation.messages.commands.CreateDepositCmd.CreateDepositCmdBuilder;
-import io.github.olegshishkin.accounting.operation.messages.commands.CreateTransferCmd.CreateTransferCmdBuilder;
-import io.github.olegshishkin.accounting.operation.messages.commands.CreateWithdrawalCmd.CreateWithdrawalCmdBuilder;
-import io.github.olegshishkin.accounting.operation.messages.commands.Header.HeaderBuilder;
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.AfterEach;
@@ -38,7 +42,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -56,16 +59,13 @@ class MessageQueueListenersTest {
   private static final MongoDBContainer DB_CONTAINER = new MongoDBContainer("mongo:5.0.8");
 
   @Autowired
-  private OperationService service;
+  private OperationService operationService;
 
   @Autowired
   private ReactiveMongoOperations ops;
 
   @Autowired
   private OperationMapper operationMapper;
-
-  @Autowired
-  private TransferMapper transferMapper;
 
   @InjectMocks
   private MessageQueueListeners listeners;
@@ -80,7 +80,7 @@ class MessageQueueListenersTest {
     var publisher = Mockito.mock(ApplicationEventPublisher.class);
     doNothing().when(publisher).publishEvent(any());
 
-    listeners = new MessageQueueListeners(service, operationMapper, transferMapper, publisher);
+    listeners = new MessageQueueListeners(operationService, operationMapper, publisher);
   }
 
   @AfterEach
@@ -97,15 +97,24 @@ class MessageQueueListenersTest {
     var savedAccount = ops.insert(account).block();
     assert savedAccount != null;
 
-    var cmd = new CreateDepositCmdBuilder()
-        .withHeader(new HeaderBuilder().withId("id1").withTime(Instant.now()).build())
+    var cmd = new ApplyTransactionCmdBuilder()
+        .withHeader(
+            new HeaderBuilder()
+                .withId("id1")
+                .withCorrelationId("corr1")
+                .withTime(Instant.now())
+                .build())
+        .withDate(LocalDate.now())
         .withTransactionId("transId1")
-        .withAccountId(savedAccount.getId())
-        .withAmount(BigDecimal.valueOf(2.3))
         .build();
+    cmd.getEntries().add(new EntryBuilder()
+        .withAccountId(savedAccount.getId())
+        .withType(Type.DEPOSIT)
+        .withAmount(BigDecimal.valueOf(2.3))
+        .build());
 
     // when
-    listeners.handle(operationMapper::map, service::deposit, cmd).block();
+    listeners.handle(operationMapper::map, operationService::execute, cmd).block();
 
     // then
     StepVerifier.create(ops.query(Operation.class).all())
@@ -130,15 +139,24 @@ class MessageQueueListenersTest {
     var savedAccount = ops.insert(account).block();
     assert savedAccount != null;
 
-    var cmd = new CreateWithdrawalCmdBuilder()
-        .withHeader(new HeaderBuilder().withId("id1").withTime(Instant.now()).build())
+    var cmd = new ApplyTransactionCmdBuilder()
+        .withHeader(
+            new HeaderBuilder()
+                .withId("id1")
+                .withCorrelationId("corr1")
+                .withTime(Instant.now())
+                .build())
+        .withDate(LocalDate.now())
         .withTransactionId("transId1")
-        .withAccountId(savedAccount.getId())
-        .withAmount(BigDecimal.valueOf(2.2))
         .build();
+    cmd.getEntries().add(new EntryBuilder()
+        .withAccountId(savedAccount.getId())
+        .withType(Type.WITHDRAWAL)
+        .withAmount(BigDecimal.valueOf(2.2))
+        .build());
 
     // when
-    listeners.handle(operationMapper::map, service::deposit, cmd).block();
+    listeners.handle(operationMapper::map, operationService::execute, cmd).block();
 
     // then
     StepVerifier.create(ops.query(Operation.class).all())
@@ -168,16 +186,29 @@ class MessageQueueListenersTest {
     var accounts = ops.insertAll(List.of(source, dest)).toStream().toList();
     assert accounts.size() == 2;
 
-    var cmd = new CreateTransferCmdBuilder()
-        .withHeader(new HeaderBuilder().withId("id1").withTime(Instant.now()).build())
+    var cmd = new ApplyTransactionCmdBuilder()
+        .withHeader(
+            new HeaderBuilder()
+                .withId("id1")
+                .withCorrelationId("corr1")
+                .withTime(Instant.now())
+                .build())
+        .withDate(LocalDate.now())
         .withTransactionId("transId1")
-        .withFromAccountId(source.getId())
-        .withToAccountId(dest.getId())
-        .withAmount(BigDecimal.valueOf(0.52))
         .build();
+    cmd.getEntries().add(new EntryBuilder()
+        .withAccountId(source.getId())
+        .withType(Type.WITHDRAWAL)
+        .withAmount(BigDecimal.valueOf(0.52))
+        .build());
+    cmd.getEntries().add(new EntryBuilder()
+        .withAccountId(dest.getId())
+        .withType(Type.DEPOSIT)
+        .withAmount(BigDecimal.valueOf(0.52))
+        .build());
 
     // when
-    listeners.handle(transferMapper::map, service::transfer, cmd).block();
+    listeners.handle(operationMapper::map, operationService::execute, cmd).block();
 
     // then
     var operations = ops.query(Operation.class)
@@ -212,24 +243,29 @@ class MessageQueueListenersTest {
   @TestConfiguration
   static class MessageQueueListenersTestConfig {
 
-    @Primary
     @Bean
     public OperationMapper operationMapper() {
       return new OperationMapperImpl();
     }
 
-    @Primary
     @Bean
-    public TransferMapper transferMapper(OperationMapper operationMapper) {
-      return new TransferMapperImpl(operationMapper);
+    public AccountMapper accountMapper() {
+      return new AccountMapperImpl();
     }
 
-    @Primary
+    @Bean
+    public AccountService accountService(ReactiveMongoOperations ops,
+        AccountRepository repository,
+        AccountMapper accountMapper) {
+      return new AccountServiceImpl(repository, accountMapper, ops);
+    }
+
     @Bean
     public OperationService operationService(ReactiveMongoOperations ops,
         OperationRepository operationRepository,
-        OperationMapper mapper) {
-      return new OperationServiceImpl(ops, operationRepository, mapper);
+        OperationMapper mapper,
+        AccountService accountService) {
+      return new OperationServiceImpl(ops, operationRepository, mapper, accountService);
     }
   }
 }
